7e1e19d124ecdc7c6e5d3eec5358ad4e95f34ee8
Matheus Couto <matheusccouto@gmail.com>
2021-05-12 20:08:36 -0300

    Develop sales endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,33 @@
+""" API for time series predictions for the Superstore dataset. """
+
+import json
+import os
+
 import flask
+import prophet.serialize
+
+THIS_FOLDER = os.path.dirname(__file__)
+ROOT_FOLDER = os.path.join(*os.path.split(THIS_FOLDER)[:-1])
+MODEL_PATH = os.path.join(ROOT_FOLDER, "model", "model.json")
 
 app = flask.Flask(__name__)
 
-
-@app.route("/")
-def home():
-    """ Home page. """
-    return "<h1>Superstore API</h1>Time series predictions for the Superstore dataset"
+with open(MODEL_PATH) as file:
+    model = prophet.serialize.model_from_json(json.load(file))
 
 
-@app.route("/v1/orders/")
+def _forecast_sales(periods):
+    """Forecast sales."""
+    future = model.make_future_dataframe(periods=periods, include_history=False)
+    forecast = model.predict(future)
+    forecast = forecast.set_index("ds")
+    forecast = forecast["yhat"]
+    forecast.index = forecast.index.astype(str)
+    return forecast.to_dict()
+
+
+@app.route("/v1/sales/")
 def orders():
-    """ Get inbound orders prediction """
-    return flask.jsonify({
-            "And now": "something completely different..."
-        })
+    """Get sales forecast."""
+    periods = flask.request.args.get("days")
+    return _forecast_sales(int(periods))
